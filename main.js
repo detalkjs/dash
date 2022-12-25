c6388ff1cfeb161f4b39890f63b261242848c841
CKY <awa@outlook.ie>
2022-12-25 15:44:27 +0800

    :tada: First Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
+const $ = cheerio.load(html);
+
+// 遍历 src/page 目录下的所有 js 文件
+function dir(_path) {
+  console.log(_path);
+  const pageDir = path.resolve(__dirname, _path);
+  const pageFiles = fs.readdirSync(pageDir);
+  console.log(pageFiles)
+  pageFiles.forEach((file) => {
+      const filePath = path.resolve(pageDir, file);
+      const fileStat = fs.statSync(filePath);
+      if (fileStat.isFile() && path.extname(filePath) === '.html') {
+          console.log("is file");
+          const fileName = path.basename(filePath, '.html');
+          const fileContent = fs.readFileSync(filePath, 'utf8');
+          $("#app").html(fileContent);
+          fs.writeFileSync("dist/" + fileName + ".html", $.html(), 'utf8');
+      } else if (!fileStat.isFile()) {
+          console.log("is dir");
+          dir(filePath);
+      }
+  });
+}
+
+dir('src/page');
+
+
+fs.writeFileSync("dist/style.css", fs.readFileSync("src/style.css"), 'utf8');
